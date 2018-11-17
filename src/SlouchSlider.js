6f602687675af99a724eff367bbfaca2843ace71
Sean Nealon <signalflowsean@gmail.com>
2018-11-17 14:24:58 -0500

    image frame out of picture, better slouching

--- src/SlouchSlider.js
+++ src/SlouchSlider.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import Webcam from 'react-webcam'; 
 import * as posenet from '@tensorflow-models/posenet';
-import { SSL_OP_NETSCAPE_DEMO_CIPHER_CHANGE_BUG } from 'constants';
 
 export default class SlouchSlider extends React.Component{
   constructor(props){ 
     super(props); 
     
     this.image = null; 
+    this.imageScaleFactor = 0.5; 
+    this.flipHorizontal = false; 
+    this.outputStride = 16; 
 
     this.state = { 
       slouch : 0, 
       isLoaded : false, 
       posenet : null,
       capture : null, 
-      img : null,
-      //POSENET CONSTANTS 
-      feedback : null,
-      imageScaleFactor : 0.5, 
-      flipHorizontal : false, 
-      outputStride : 16
+      feedback : null
     }; 
   }
   
@@ -43,42 +40,36 @@ export default class SlouchSlider extends React.Component{
   capture = () => { 
     let image = new Image(); 
 
-    const src = this.webcam.getScreenshot(); 
+    const capture = this.webcam.getScreenshot(); 
 
-    //console.log('src', src);
-    image.src = src; 
-    image.width = 600; 
-    image.height = 500; 
+    image.src = capture; 
+    image.width = 200; 
+    image.height = 200; 
   
-    //document.body.appendChild(image); 
-    this.setState({capture : src})
+    this.setState({capture})
     
-    //console.log('capture', this.state.capture);
     this.findPose(this.image);
   };
   
   findPose = (img) => {     
       this.state.posenet.estimateSinglePose(img, 
-      this.state.imageScaleFactor, 
-      this.state.flipHorizontal, 
-      this.state.outputStride)
+      this.imageScaleFactor, 
+      this.flipHorizontal, 
+      this.outputStride)
       .then((pose) => { 
         this.calculateSlouch(pose);     
       })
   }
 
   calculateSlouch = (pose) => { 
-    //console.log(pose.keypoints);
     const leftEye = pose.keypoints[0].position; 
     const rightEye = pose.keypoints[1].position; 
     const leftShoulder = pose.keypoints[4].position; 
     const rightShoulder = pose.keypoints[5].position; 
-
-    const slouch = (leftEye.y - leftShoulder.y); 
-    console.log(slouch);
+    const slouch = (((leftEye.y - leftShoulder.y) + (rightEye.y - rightShoulder.y))/2) + 34; 
+    
+    console.log('slouch', slouch);
     this.setState({slouch}); 
-    //console.log('slouch', slouch);
-    //console.log('leftEye', leftEye, 'rightEye', rightEye, 'leftShoulder', leftShoulder, 'rightShoulder', rightShoulder);
   } 
 
   onWebcamloaded = () => { 
@@ -118,11 +109,44 @@ export default class SlouchSlider extends React.Component{
           value={this.state.slouch} 
           step="1" 
           min="0" 
-          max="100"
+          max="40"
           onChange={(e) => console.log(e.currentTarget.value) }
           >
         </input>
-        <img src={this.state.capture} ref={this.setImage} width="500" height="600"></img>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <img src={this.state.capture} alt="pose" ref={this.setImage} width="200" height="200"></img>
       </div>
     ); 
   }
