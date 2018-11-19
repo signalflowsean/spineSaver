0889fbb92651ac2b855777880d70401ddd74e115
Sean Nealon <signalflowsean@gmail.com>
2018-11-19 10:19:53 -0500

    calib square is on top of webcamfeed

--- src/SlouchSlider.js
+++ src/SlouchSlider.js
@@ -2,22 +2,31 @@ import React from 'react';
 import Webcam from 'react-webcam'; 
 import * as posenet from '@tensorflow-models/posenet';
 import { Stage, Layer, Rect } from 'react-konva';
-import Konva from 'konva'; 
-
-
+import './Styles/camCalibStack.css'; 
+ 
 export default class SlouchSlider extends React.Component{
   constructor(props){ 
     super(props); 
     
-    this.image = null; 
+    //image element
+    this.image = null;
+    
+    //posenet constants
     this.imageScaleFactor = 0.5; 
     this.flipHorizontal = false; 
     this.outputStride = 16;
     this.frameRate = 150;  
 
-    this.width = 200; 
-    this.height = 200; 
+    //constant height and widths
+    this.width = 500; 
+    this.height = 500; 
 
+    //calibration rect size
+    this.sizeDecrease = 100; 
+    this.calibWidth = this.width - this.sizeDecrease; 
+    this.calibHeight = this.width - this.sizeDecrease; 
+    
+    //constants for webcam
     this.videoConstraints = { 
       width : this.width,  
       height : this.height, 
@@ -51,20 +60,19 @@ export default class SlouchSlider extends React.Component{
   }; 
 
   capture = () => { 
-    let image = new Image(); 
-
     const capture = this.webcam.getScreenshot(); 
-
-    image.src = capture; 
-    image.width = 200; 
-    image.height = 200; 
   
+    //set the screenshot to the state, set the src of html image
+    //to the capture, find the pose from the reference of the 
+    //html image element
     this.setState({capture}, this.findPose(this.image)); 
   };
   
   findPose = (img) => {     
-      this.state.posenet.estimateSinglePose(img, this.imageScaleFactor, 
-        this.flipHorizontal, this.outputStride)
+      this.state.posenet.estimateSinglePose(img, 
+        this.imageScaleFactor, 
+        this.flipHorizontal, 
+        this.outputStride)
           .then((pose) => { 
             this.calculateSlouch(pose);     
       })
@@ -75,6 +83,8 @@ export default class SlouchSlider extends React.Component{
     const rightEye = pose.keypoints[1].position; 
     const leftShoulder = pose.keypoints[4].position; 
     const rightShoulder = pose.keypoints[5].position; 
+
+    //TODO: better calculation fro slouch
     const slouch = (((leftEye.y - leftShoulder.y) + (rightEye.y - rightShoulder.y))/2) + 34; 
     
     //console.log('slouch', slouch);
@@ -98,26 +108,34 @@ export default class SlouchSlider extends React.Component{
 
     return ( 
       <div>
-        <p>{this.state.feedback}</p>
-        <Stage width={window.innerWidth} height={window.innerHeight}>
+        {/* Stage, Layer and Rect is for rendering calibration visuals */}
+        <Stage 
+          className={'calibration-stage'} 
+          width={this.width} 
+          height={this.height}>
           <Layer>
             <Rect
-              x={20}
-              y={20}
-              width={50}
-              height={50}
-              fill={'green'}
+              // id={'calibration-rect'}
+              x={this.sizeDecrease/2}
+              y={this.sizeDecrease/2}
+              width={this.calibWidth}
+              height={this.calibHeight}
+              stroke={'black'}
               shadowBlur={5}
             />
           </Layer>
         </Stage>
         <Webcam 
+          className={'webcam'}
           audio={false}
+          height={this.height}
+          width={this.width}
           screenshotFormat="image/png"
           videoConstraints={this.videoConstraints}
           onUserMedia={this.onWebcamloaded}
           ref={this.setRef}
         />
+        <p>{this.state.feedback}</p>
         <input 
           type="range" 
           name="slouchSlider" 
