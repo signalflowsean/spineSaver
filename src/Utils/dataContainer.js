c705894069940e1d2b212e7a9b9c480a43e5da12
Sean Nealon <signalflowsean@gmail.com>
2018-11-20 10:37:30 -0500

    fixed slouchData length issue for the moment

--- src/Utils/dataContainer.js
+++ src/Utils/dataContainer.js
@@ -4,20 +4,22 @@ let slouchData = [];
 
 export default function DataContainer(data) { 
   slouchData.push(data); 
-  
+  //TODO: there's a bit of odd asyncronicity
+  //console.log(slouchData.length);
   if (slouchData.length === size){ 
-
     fetch(`${API_URL}/slouchData`, { 
       method: 'post',
       headers: {'Content-Type':'application/json'},
       body: JSON.stringify({slouchData})
-    }).then(res => { 
+    }).then(res => {
       return res.json(); 
     }).then(slouchData => {  
-      console.log('slouch data', slouchData);
+      //console.log('Slouch Data: ', JSON.stringify(slouchData)); 
     }).catch(error => { 
       console.log('Error:', error);
-    });   
+    });
+    
+    slouchData = [];    
   }
 }
 
