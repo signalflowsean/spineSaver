04beb688b45f912c42fa7e8f04126e9b8ec139e2
Sean Nealon <signalflowsean@gmail.com>
2018-11-24 12:23:52 -0500

    can create a new user

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,5 @@
 import React from 'react'; 
+import {API_BASE_URL} from '../config';
 import '../Styles/signup.css'; 
 
 export default class SignUp extends React.Component{ 
@@ -10,18 +11,33 @@ export default class SignUp extends React.Component{
       password : '', 
       email : '', 
     }; 
-    
-    this.handleFullnameChange = this.handleFullnameChange.bind(this); 
-    this.handleUsernameChange = this.handleUsernameChange.bind(this); 
-    this.hanldePasswordChange = this.hanldePasswordChange.bind(this); 
-    this.handleEmailChange = this.handleEmailChange.bind(this); 
 
   }
 
   handleSubmit(e){ 
-    e.preventDefault(); 
- 
-  
+
+    const user = {
+      fullname : this.state.fullname, 
+      username : this.state.username, 
+      password : this.state.password, 
+    }; 
+    //console.log(user); 
+    this.postSubmitData(user);   
+  }
+
+  postSubmitData(user){
+    fetch(`${API_BASE_URL}/signup`, { 
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify(user)
+    }).then(res => {
+      //console.log('res', res);
+      return res.json(); 
+    }).then(signup => {  
+      console.log('Signup: ', JSON.stringify(signup)); 
+    }).catch(error => { 
+      console.log('Error:', error);
+    });
 
   }
 
@@ -49,14 +65,14 @@ export default class SignUp extends React.Component{
     return (
       <form onSubmit={this.handleSubmit}>
         <label>Full Name</label>
-        <input type="text" placeholder="Jane Doe" value={this.state.fullname} onChange={this.handleFullnameChange}></input>
+        <input type="text" placeholder="Jane Doe" value={this.state.fullname} onChange={(e) => this.handleFullnameChange(e)}></input>
         <label>Username</label>
-        <input type="text" placeholder="JayD23" value={this.state.username} onChange={this.handleUsernameChange}></input>
+        <input type="text" placeholder="JayD23" value={this.state.username} onChange={(e) => this.handleUsernameChange(e)}></input>
         <label>Password</label>
-        <input type="password" placeholder="password123" value={this.state.password} onChange={this.hanldePasswordChange}></input>
+        <input type="password" placeholder="password123" value={this.state.password} onChange={(e) => this.hanldePasswordChange(e)}></input>
         <label>Email</label>
-        <input type="email" placeholder="jdoe23@fawn.com" value={this.state.email} onChange={this.handleEmailChange}></input>
-        <input type="submit" ></input>
+        <input type="email" placeholder="jdoe23@fawn.com" value={this.state.email} onChange={(e) => this.handleEmailChange(e)}></input>
+        <input type="button" value="submit" onClick={() => this.handleSubmit()}></input>
       </form>
     ); 
   }
