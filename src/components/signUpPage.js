6b29faba8f7b24f98e409f6b1399702e93edea23
Sean Nealon <signalflowsean@gmail.com>
2018-12-07 17:36:57 -0500

    some stylez

--- src/components/signUpPage.js
+++ src/components/signUpPage.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import {connect} from 'react-redux'; 
-import {Link, Redirect} from 'react-router-dom'; 
+import {Redirect} from 'react-router-dom'; 
+import '../Styles/signup.css'; 
 
 import SignUpForm from './signUpForm'; 
 
@@ -13,9 +14,13 @@ export function SignUpPage(props) {
 
   return (
     <div>
-      <h2>SignUp</h2>
-      <Link to="/">Home</Link>
-      <SignUpForm></SignUpForm>
+      <header>
+        <h2>SignUp</h2>
+        {/* <Link className="icon" to="/">
+          <img src="https://image.flaticon.com/icons/svg/25/25694.svg" alt="home"></img>
+        </Link> */}
+      </header>
+      <SignUpForm className="signup"></SignUpForm>
     </div>
   ); 
 }
